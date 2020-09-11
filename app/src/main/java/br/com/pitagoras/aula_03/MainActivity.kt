d7b60de3c25689d78b5b2298962e8b88c11d4852
Fabricio Valadares <fabricio.valadares@gmail.com>
2020-09-11 16:31:13 -0300

    Final commit.
    This is an example used in mobile development class, showing how to use the seekbar, and how to use the constraint layout.

--- app/src/main/java/br/com/pitagoras/aula_03/MainActivity.kt
+++ app/src/main/java/br/com/pitagoras/aula_03/MainActivity.kt
@@ -1,12 +1,39 @@
 package br.com.pitagoras.aula_03
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initTxServ()
+        initSeekBar()
+    }
+
+    // Exemplo de uso da seekbar
+    private fun initSeekBar() {
+        seekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    tv_tx_serv.text = progress.toString()
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+            }
+        )
+    }
+
+    private fun initTxServ() {
+        tv_tx_serv.text = seekBar.progress.toString()
     }
 }
 
